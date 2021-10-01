805aaa58a89c733cf0356fc28c04441b9c2d9668
AhmadAttalhaoui <61194391+AhmadAttalhaoui@users.noreply.github.com>
2021-10-01 21:26:25 +0200

    code

---
+++ Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace SalesTaxCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbItem = new System.Windows.Forms.Label();
+            this.tbItem1 = new System.Windows.Forms.TextBox();
+            this.tbItem2 = new System.Windows.Forms.TextBox();
+            this.tbItem3 = new System.Windows.Forms.TextBox();
+            this.tbItem4 = new System.Windows.Forms.TextBox();
+            this.lbQuantity = new System.Windows.Forms.Label();
+            this.nudQuantity1 = new System.Windows.Forms.NumericUpDown();
+            this.nudQuantity2 = new System.Windows.Forms.NumericUpDown();
+            this.nudQuantity3 = new System.Windows.Forms.NumericUpDown();
+            this.nudQuantit4 = new System.Windows.Forms.NumericUpDown();
+            this.lbPriceEach = new System.Windows.Forms.Label();
+            this.tbPriceEach1 = new System.Windows.Forms.TextBox();
+            this.tbPriceEach2 = new System.Windows.Forms.TextBox();
+            this.tbPriceEach3 = new System.Windows.Forms.TextBox();
+            this.tbPriceEach4 = new System.Windows.Forms.TextBox();
+            this.lbExtPrice = new System.Windows.Forms.Label();
+            this.tbExtPrice1 = new System.Windows.Forms.TextBox();
+            this.tbExtPrice2 = new System.Windows.Forms.TextBox();
+            this.tbExtPrice3 = new System.Windows.Forms.TextBox();
+            this.tbExtPrice4 = new System.Windows.Forms.TextBox();
+            this.btCalculate = new System.Windows.Forms.Button();
+            this.lbSubtotal = new System.Windows.Forms.Label();
+            this.lbTaxRate = new System.Windows.Forms.Label();
+            this.lbSalesTax = new System.Windows.Forms.Label();
+            this.lbShipping = new System.Windows.Forms.Label();
+            this.lbGrandTotal = new System.Windows.Forms.Label();
+            this.tbSubtotal = new System.Windows.Forms.TextBox();
+            this.tbTaxRate = new System.Windows.Forms.TextBox();
+            this.tbSalesTax = new System.Windows.Forms.TextBox();
+            this.tbShipping = new System.Windows.Forms.TextBox();
+            this.tbGrandTotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantit4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbItem
+            // 
+            this.lbItem.AutoSize = true;
+            this.lbItem.Location = new System.Drawing.Point(12, 48);
+            this.lbItem.Name = "lbItem";
+            this.lbItem.Size = new System.Drawing.Size(39, 20);
+            this.lbItem.TabIndex = 0;
+            this.lbItem.Text = "Item";
+            // 
+            // tbItem1
+            // 
+            this.tbItem1.Location = new System.Drawing.Point(12, 71);
+            this.tbItem1.Name = "tbItem1";
+            this.tbItem1.Size = new System.Drawing.Size(125, 27);
+            this.tbItem1.TabIndex = 1;
+            // 
+            // tbItem2
+            // 
+            this.tbItem2.Location = new System.Drawing.Point(12, 104);
+            this.tbItem2.Name = "tbItem2";
+            this.tbItem2.Size = new System.Drawing.Size(125, 27);
+            this.tbItem2.TabIndex = 2;
+            // 
+            // tbItem3
+            // 
+            this.tbItem3.Location = new System.Drawing.Point(12, 137);
+            this.tbItem3.Name = "tbItem3";
+            this.tbItem3.Size = new System.Drawing.Size(125, 27);
+            this.tbItem3.TabIndex = 3;
+            // 
+            // tbItem4
+            // 
+            this.tbItem4.Location = new System.Drawing.Point(12, 170);
+            this.tbItem4.Name = "tbItem4";
+            this.tbItem4.Size = new System.Drawing.Size(125, 27);
+            this.tbItem4.TabIndex = 4;
+            // 
+            // lbQuantity
+            // 
+            this.lbQuantity.AutoSize = true;
+            this.lbQuantity.Location = new System.Drawing.Point(163, 48);
+            this.lbQuantity.Name = "lbQuantity";
+            this.lbQuantity.Size = new System.Drawing.Size(65, 20);
+            this.lbQuantity.TabIndex = 5;
+            this.lbQuantity.Text = "Quantity";
+            // 
+            // nudQuantity1
+            // 
+            this.nudQuantity1.Location = new System.Drawing.Point(163, 71);
+            this.nudQuantity1.Name = "nudQuantity1";
+            this.nudQuantity1.Size = new System.Drawing.Size(50, 27);
+            this.nudQuantity1.TabIndex = 6;
+            // 
+            // nudQuantity2
+            // 
+            this.nudQuantity2.Location = new System.Drawing.Point(163, 104);
+            this.nudQuantity2.Name = "nudQuantity2";
+            this.nudQuantity2.Size = new System.Drawing.Size(50, 27);
+            this.nudQuantity2.TabIndex = 7;
+            // 
+            // nudQuantity3
+            // 
+            this.nudQuantity3.Location = new System.Drawing.Point(163, 137);
+            this.nudQuantity3.Name = "nudQuantity3";
+            this.nudQuantity3.Size = new System.Drawing.Size(50, 27);
+            this.nudQuantity3.TabIndex = 8;
+            // 
+            // nudQuantit4
+            // 
+            this.nudQuantit4.Location = new System.Drawing.Point(163, 170);
+            this.nudQuantit4.Name = "nudQuantit4";
+            this.nudQuantit4.Size = new System.Drawing.Size(50, 27);
+            this.nudQuantit4.TabIndex = 9;
+            // 
+            // lbPriceEach
+            // 
+            this.lbPriceEach.AutoSize = true;
+            this.lbPriceEach.Location = new System.Drawing.Point(251, 48);
+            this.lbPriceEach.Name = "lbPriceEach";
+            this.lbPriceEach.Size = new System.Drawing.Size(76, 20);
+            this.lbPriceEach.TabIndex = 10;
+            this.lbPriceEach.Text = "Price Each";
+            // 
+            // tbPriceEach1
+            // 
+            this.tbPriceEach1.Location = new System.Drawing.Point(251, 71);
+            this.tbPriceEach1.Name = "tbPriceEach1";
+            this.tbPriceEach1.Size = new System.Drawing.Size(69, 27);
+            this.tbPriceEach1.TabIndex = 11;
+            // 
+            // tbPriceEach2
+            // 
+            this.tbPriceEach2.Location = new System.Drawing.Point(251, 104);
+            this.tbPriceEach2.Name = "tbPriceEach2";
+            this.tbPriceEach2.Size = new System.Drawing.Size(69, 27);
+            this.tbPriceEach2.TabIndex = 12;
+            // 
+            // tbPriceEach3
+            // 
+            this.tbPriceEach3.Location = new System.Drawing.Point(251, 137);
+            this.tbPriceEach3.Name = "tbPriceEach3";
+            this.tbPriceEach3.Size = new System.Drawing.Size(69, 27);
+            this.tbPriceEach3.TabIndex = 13;
+            // 
+            // tbPriceEach4
+            // 
+            this.tbPriceEach4.Location = new System.Drawing.Point(251, 170);
+            this.tbPriceEach4.Name = "tbPriceEach4";
+            this.tbPriceEach4.Size = new System.Drawing.Size(69, 27);
+            this.tbPriceEach4.TabIndex = 14;
+            // 
+            // lbExtPrice
+            // 
+            this.lbExtPrice.AutoSize = true;
+            this.lbExtPrice.Location = new System.Drawing.Point(358, 47);
+            this.lbExtPrice.Name = "lbExtPrice";
+            this.lbExtPrice.Size = new System.Drawing.Size(68, 20);
+            this.lbExtPrice.TabIndex = 15;
+            this.lbExtPrice.Text = "Ext. Price";
+            // 
+            // tbExtPrice1
+            // 
+            this.tbExtPrice1.CausesValidation = false;
+            this.tbExtPrice1.Enabled = false;
+            this.tbExtPrice1.Location = new System.Drawing.Point(358, 70);
+            this.tbExtPrice1.Name = "tbExtPrice1";
+            this.tbExtPrice1.Size = new System.Drawing.Size(89, 27);
+            this.tbExtPrice1.TabIndex = 16;
+            // 
+            // tbExtPrice2
+            // 
+            this.tbExtPrice2.Enabled = false;
+            this.tbExtPrice2.Location = new System.Drawing.Point(358, 103);
+            this.tbExtPrice2.Name = "tbExtPrice2";
+            this.tbExtPrice2.Size = new System.Drawing.Size(89, 27);
+            this.tbExtPrice2.TabIndex = 17;
+            // 
+            // tbExtPrice3
+            // 
+            this.tbExtPrice3.Enabled = false;
+            this.tbExtPrice3.Location = new System.Drawing.Point(358, 136);
+            this.tbExtPrice3.Name = "tbExtPrice3";
+            this.tbExtPrice3.Size = new System.Drawing.Size(89, 27);
+            this.tbExtPrice3.TabIndex = 18;
+            // 
+            // tbExtPrice4
+            // 
+            this.tbExtPrice4.Enabled = false;
+            this.tbExtPrice4.Location = new System.Drawing.Point(358, 169);
+            this.tbExtPrice4.Name = "tbExtPrice4";
+            this.tbExtPrice4.Size = new System.Drawing.Size(89, 27);
+            this.tbExtPrice4.TabIndex = 19;
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(83, 282);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(94, 29);
+            this.btCalculate.TabIndex = 20;
+            this.btCalculate.Text = "Calculate";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            // 
+            // lbSubtotal
+            // 
+            this.lbSubtotal.AutoSize = true;
+            this.lbSubtotal.Location = new System.Drawing.Point(235, 239);
+            this.lbSubtotal.Name = "lbSubtotal";
+            this.lbSubtotal.Size = new System.Drawing.Size(65, 20);
+            this.lbSubtotal.TabIndex = 21;
+            this.lbSubtotal.Text = "Subtotal";
+            // 
+            // lbTaxRate
+            // 
+            this.lbTaxRate.AutoSize = true;
+            this.lbTaxRate.Location = new System.Drawing.Point(235, 272);
+            this.lbTaxRate.Name = "lbTaxRate";
+            this.lbTaxRate.Size = new System.Drawing.Size(64, 20);
+            this.lbTaxRate.TabIndex = 22;
+            this.lbTaxRate.Text = "Tax Rate";
+            // 
+            // lbSalesTax
+            // 
+            this.lbSalesTax.AutoSize = true;
+            this.lbSalesTax.Location = new System.Drawing.Point(235, 305);
+            this.lbSalesTax.Name = "lbSalesTax";
+            this.lbSalesTax.Size = new System.Drawing.Size(68, 20);
+            this.lbSalesTax.TabIndex = 23;
+            this.lbSalesTax.Text = "Sales Tax";
+            // 
+            // lbShipping
+            // 
+            this.lbShipping.AutoSize = true;
+            this.lbShipping.Location = new System.Drawing.Point(235, 338);
+            this.lbShipping.Name = "lbShipping";
+            this.lbShipping.Size = new System.Drawing.Size(68, 20);
+            this.lbShipping.TabIndex = 24;
+            this.lbShipping.Text = "Shipping";
+            // 
+            // lbGrandTotal
+            // 
+            this.lbGrandTotal.AutoSize = true;
+            this.lbGrandTotal.Location = new System.Drawing.Point(235, 371);
+            this.lbGrandTotal.Name = "lbGrandTotal";
+            this.lbGrandTotal.Size = new System.Drawing.Size(86, 20);
+            this.lbGrandTotal.TabIndex = 25;
+            this.lbGrandTotal.Text = "Grand Total";
+            // 
+            // tbSubtotal
+            // 
+            this.tbSubtotal.Enabled = false;
+            this.tbSubtotal.Location = new System.Drawing.Point(358, 236);
+            this.tbSubtotal.Name = "tbSubtotal";
+            this.tbSubtotal.Size = new System.Drawing.Size(89, 27);
+            this.tbSubtotal.TabIndex = 26;
+            // 
+            // tbTaxRate
+            // 
+            this.tbTaxRate.Location = new System.Drawing.Point(358, 269);
+            this.tbTaxRate.Name = "tbTaxRate";
+            this.tbTaxRate.Size = new System.Drawing.Size(89, 27);
+            this.tbTaxRate.TabIndex = 27;
+            // 
+            // tbSalesTax
+            // 
+            this.tbSalesTax.Enabled = false;
+            this.tbSalesTax.Location = new System.Drawing.Point(358, 302);
+            this.tbSalesTax.Name = "tbSalesTax";
+            this.tbSalesTax.Size = new System.Drawing.Size(89, 27);
+            this.tbSalesTax.TabIndex = 28;
+            // 
+            // tbShipping
+            // 
+            this.tbShipping.Location = new System.Drawing.Point(358, 335);
+            this.tbShipping.Name = "tbShipping";
+            this.tbShipping.Size = new System.Drawing.Size(89, 27);
+            this.tbShipping.TabIndex = 29;
+            // 
+            // tbGrandTotal
+            // 
+            this.tbGrandTotal.Enabled = false;
+            this.tbGrandTotal.Location = new System.Drawing.Point(358, 368);
+            this.tbGrandTotal.Name = "tbGrandTotal";
+            this.tbGrandTotal.Size = new System.Drawing.Size(89, 27);
+            this.tbGrandTotal.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbGrandTotal);
+            this.Controls.Add(this.tbShipping);
+            this.Controls.Add(this.tbSalesTax);
+            this.Controls.Add(this.tbTaxRate);
+            this.Controls.Add(this.tbSubtotal);
+            this.Controls.Add(this.lbGrandTotal);
+            this.Controls.Add(this.lbShipping);
+            this.Controls.Add(this.lbSalesTax);
+            this.Controls.Add(this.lbTaxRate);
+            this.Controls.Add(this.lbSubtotal);
+            this.Controls.Add(this.btCalculate);
+            this.Controls.Add(this.tbExtPrice4);
+            this.Controls.Add(this.tbExtPrice3);
+            this.Controls.Add(this.tbExtPrice2);
+            this.Controls.Add(this.tbExtPrice1);
+            this.Controls.Add(this.lbExtPrice);
+            this.Controls.Add(this.tbPriceEach4);
+            this.Controls.Add(this.tbPriceEach3);
+            this.Controls.Add(this.tbPriceEach2);
+            this.Controls.Add(this.tbPriceEach1);
+            this.Controls.Add(this.lbPriceEach);
+            this.Controls.Add(this.nudQuantit4);
+            this.Controls.Add(this.nudQuantity3);
+            this.Controls.Add(this.nudQuantity2);
+            this.Controls.Add(this.nudQuantity1);
+            this.Controls.Add(this.lbQuantity);
+            this.Controls.Add(this.tbItem4);
+            this.Controls.Add(this.tbItem3);
+            this.Controls.Add(this.tbItem2);
+            this.Controls.Add(this.tbItem1);
+            this.Controls.Add(this.lbItem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantit4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbItem;
+        private System.Windows.Forms.TextBox tbItem1;
+        private System.Windows.Forms.TextBox tbItem2;
+        private System.Windows.Forms.TextBox tbItem3;
+        private System.Windows.Forms.TextBox tbItem4;
+        private System.Windows.Forms.Label lbQuantity;
+        private System.Windows.Forms.NumericUpDown nudQuantity1;
+        private System.Windows.Forms.NumericUpDown nudQuantity2;
+        private System.Windows.Forms.NumericUpDown nudQuantity3;
+        private System.Windows.Forms.NumericUpDown nudQuantit4;
+        private System.Windows.Forms.Label lbPriceEach;
+        private System.Windows.Forms.TextBox tbPriceEach1;
+        private System.Windows.Forms.TextBox tbPriceEach2;
+        private System.Windows.Forms.TextBox tbPriceEach3;
+        private System.Windows.Forms.TextBox tbPriceEach4;
+        private System.Windows.Forms.Label lbExtPrice;
+        private System.Windows.Forms.TextBox tbExtPrice1;
+        private System.Windows.Forms.TextBox tbExtPrice2;
+        private System.Windows.Forms.TextBox tbExtPrice3;
+        private System.Windows.Forms.TextBox tbExtPrice4;
+        private System.Windows.Forms.Button btCalculate;
+        private System.Windows.Forms.Label lbSubtotal;
+        private System.Windows.Forms.Label lbTaxRate;
+        private System.Windows.Forms.Label lbSalesTax;
+        private System.Windows.Forms.Label lbShipping;
+        private System.Windows.Forms.Label lbGrandTotal;
+        private System.Windows.Forms.TextBox tbSubtotal;
+        private System.Windows.Forms.TextBox tbTaxRate;
+        private System.Windows.Forms.TextBox tbSalesTax;
+        private System.Windows.Forms.TextBox tbShipping;
+        private System.Windows.Forms.TextBox tbGrandTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesTaxCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalculate_Click(object sender, EventArgs e)
+        {
+            decimal ExtPrice1 = nudQuantity1.Value * int.Parse(tbPriceEach1.Text);
+            decimal ExtPrice2 = nudQuantity2.Value * int.Parse(tbPriceEach2.Text);
+            decimal ExtPrice3 = nudQuantity3.Value * int.Parse(tbPriceEach3.Text);
+            decimal ExtPrice4 = nudQuantit4.Value *int.Parse(tbPriceEach4.Text);
+
+          
+
+        }
+    }
+}
